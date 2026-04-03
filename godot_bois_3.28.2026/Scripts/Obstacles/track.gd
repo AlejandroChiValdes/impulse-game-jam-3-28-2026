32599fec2058a47f64052a04854471e65befa14e
hd-tran <36580646+hd-tran@users.noreply.github.com>
2026-04-02 23:24:49 -0700

    WIP track fix, rewrote some of the base logic that was bugged with animation speed ups and attempted to deal with track offsets deviating during time shifts
    - fixed issue with timeshifting not properly applying animation speed ups
    - attempted, but not fully fixed: issue with platforms deviating when timeshifting during a platforms delayed movement
      - still sometimes deviates and easier to deviate for longer delays, but short or no delays have minimal impact

--- godot_bois_3.28.2026/Scripts/Obstacles/track.gd
+++ godot_bois_3.28.2026/Scripts/Obstacles/track.gd
@@ -1,8 +1,10 @@
 extends Path2D
 
+@export var debug_view: bool = false
+
 @onready var animation_player_ref: AnimationPlayer = $AnimationPlayer
 @export var animation_scale: float
-@export var ping_pong_delay: float = 0.0
+@export var base_ping_pong_delay: float = 0.0
 @export var START_POINT_SECONDS: float = 0.0
 
 @export var speed_map: Array[float]
@@ -11,48 +13,70 @@ var current_speed_type: GameManager.TimeControl = 0
 @onready var game_manager = GameManager
 
 var start_delay: bool = false
-var current_delay_timer: float = 0.0
+#var shifted_delay_timer: float = 0.0
+#var shifted_ping_pong_delay: float
+
+var base_delay_timer: float = 0.0
 
 @onready var track_follow: PathFollow2D = $PlatformFollow
 var animation_name = "moving_platform"
 var is_playback_reversed: bool = false
 
+var animation_playback_rate: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	game_manager.time_control_changed.connect(_on_time_control_changed)
 
 	animation_player_ref.current_animation = animation_name
-	animation_player_ref.advance(START_POINT_SECONDS)
+	animation_player_ref.seek(START_POINT_SECONDS)
+
+	animation_playback_rate = animation_scale * speed_map[current_speed_type]
+
+	animation_player_ref.play(animation_name, -1, animation_playback_rate, false )
+
+	if debug_view:
+		var debug_root: Control = $PlatformFollow/AnimatableBody2D/DebugUI
+		debug_root.visible = true
 	return
 
 
 func _physics_process(delta: float) -> void:
+	if debug_view:
+		_debug_options()
+	
+	if is_playback_reversed:
+		animation_playback_rate = -1 * animation_scale * speed_map[current_speed_type]
+	else:
+		animation_playback_rate = animation_scale * speed_map[current_speed_type]
 
 	if start_delay:
-		if current_delay_timer >= ping_pong_delay:
-			is_playback_reversed = not is_playback_reversed
-			animation_scale = -1 * animation_scale
-			animation_player_ref.speed_scale = animation_scale * speed_map[current_speed_type]
-			animation_player_ref.play(animation_name, -1, speed_map[current_speed_type], is_playback_reversed)
-
-			current_delay_timer = 0.0
+		if base_delay_timer >= base_ping_pong_delay:
+			animation_player_ref.play(animation_name, -1, animation_playback_rate, is_playback_reversed)
+			base_delay_timer = 0.0
 			start_delay = false
-		current_delay_timer += delta * speed_map[current_speed_type]
+		else:
+			base_delay_timer += delta * speed_map[current_speed_type]
 	else:
-		animation_player_ref.speed_scale = animation_scale * speed_map[current_speed_type]
-
+		animation_player_ref.play(animation_name, -1, animation_playback_rate, is_playback_reversed)
+	
 	return
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == animation_name:
-		if ping_pong_delay > 0.0:
+		is_playback_reversed = not is_playback_reversed
+		if base_ping_pong_delay > 0.0:
 			start_delay = true
 	return
 
 
 func _on_time_control_changed(new_time_control: GameManager.TimeControl):
 	current_speed_type = new_time_control
-	animation_player_ref.speed_scale = animation_scale * speed_map[current_speed_type]
+	return
+
+
+func _debug_options():
+	var debug_playback_rate_label: Label = $"PlatformFollow/AnimatableBody2D/DebugUI/Playback Rate"
+	debug_playback_rate_label.text = str(animation_playback_rate)
 	return
