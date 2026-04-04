b9f283ec186a861d8e141895490dd5ebc3f22282
Tristan Ligtvoet <36016624+Tligtvoet@users.noreply.github.com>
2026-04-04 15:16:56 -0700

    Added Restart Functionality
    On end level screen

--- godot_bois_3.28.2026/Scripts/EndGame/exit_script.gd
+++ godot_bois_3.28.2026/Scripts/EndGame/exit_script.gd
@@ -4,5 +4,9 @@ func _ready() -> void:
 	visible = false
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
-func _on_button_pressed() -> void:
+func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_restart_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
